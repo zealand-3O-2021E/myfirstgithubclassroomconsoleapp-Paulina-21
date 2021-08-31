eb977e15f2c73dc3638baab758f50eea4a1faaac
Paula Catalina Curca <paul6465@edu.easj.dk>
2021-08-31 14:26:54 +0200

    first

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFirstGitHubClassRoomConsoleApp
+{
+    public class Demo
+    {
+        public Demo(int start, int finish)
+        {
+            StartUp = start;
+            Finish = finish;
+        }
+        public int StartUp { get; set; }
+        public int Finish { set; get; }
+    }
+}
